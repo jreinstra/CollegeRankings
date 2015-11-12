44863e7a2cec78b93ba4919b6357af749c83aef6
yohanstra <jreinstra@gmail.com>
2015-11-11 20:37:27 -0800

    Added live rankings calculation

--- main.js
+++ main.js
@@ -4,16 +4,9 @@ var values = {};
 var data = null;
 initDatabase(function(loadedData) {
     data = JSON.parse(loadedData);
-    displayColleges(data);
     
     for(var i in data[0]) {
-        if(
-            data[0][i] &&
-            i != "Setting" &&
-            i != "Athletic Association" &&
-            i != "CollegeName" &&
-            i != "School Type"
-          ) {
+        if(data[0][i] && i != "CollegeName") {
             var id = "s_" + i.replace(/ /g, "-");
             $("#form").append('<div><h5>' + i + '</h5><span id="' + id + '"></span></div><span id="' + id + '_val">5</span><br/>');
             sliders[id] = $("#" + id).slider();
@@ -21,6 +14,7 @@ initDatabase(function(loadedData) {
             $("#" + id).change(formChanged);
         }
     }
+    rankColleges();
 });
 
 function initDatabase(callback) {
@@ -42,18 +36,44 @@ function formChanged(e) {
     var value = sliders[id].slider('getValue');
     values[id] = value;
     $("#" + id + "_val").html(value);
+    rankColleges();
 }
 
-function displayColleges(data) {
+function displayColleges() {
+    $("#colleges").html("");
     for(var i = 0; i < 10; i++) {
         $("#colleges").append(
             '<div class="panel panel-default">' + 
                 '<div class="panel-body"><h4>' + 
                     (parseInt(i) + 1) + '. ' + data[i]["CollegeName"] + 
-                '</h4></div>' +
+                '</h4><p>Weighted rank: ' + Math.round(data[i]["WeightedRank"] * 10) / 10.0 + '</p></div>' +
             '</div>'
         );   
     }
+}
+
+function rankColleges() {
+    for(var i = 0; i < data.length; i++) {
+        var totalRank = 0;
+        var totalPoints = 0;
+        
+        for(var valueID in values) {
+            var value = values[valueID];
+            var dataID = valueID.substring(2, valueID.length).replace(/-/g, " ");
+            
+            totalRank += data[i][dataID] * value
+            totalPoints += value
+        }
+        
+        data[i]["WeightedRank"] = totalRank * 1.0 / totalPoints;
+        //console.log(data[i]["CollegeName"] + ": " + (totalRank * 1.0 / totalPoints));
+    }
+    
+    data.sort(function(a, b) {
+        return a.WeightedRank - b.WeightedRank;
+    });
+    
+    displayColleges(data);
 }
 
 function httpGetAsync(theUrl, callback) {
